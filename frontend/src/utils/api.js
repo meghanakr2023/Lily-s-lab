bceea64ae9dce6d8c03777c745f4fc0609e50199
Meghana K R <meghanakr2004@gmail.com>
2026-06-15 16:18:48 +0530

    Fix API URL

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://lily-s-lab.onrender.com',
+  baseURL: 'https://lily-s-lab.onrender.com/api',
   timeout: 15000,
   headers: {
     'Content-Type': 'application/json',
